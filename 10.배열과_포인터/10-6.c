71b225eea8c7604be94901b9d5665936c2496385
enrhd24 <enrhd567@gmail.com>
2023-08-21 19:40:48 +0900

    혼공 C cheapter 10 added

---
+++ 10.배열과_포인터/10-6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void print_ary(int *pa, int size);
+
+int main(void){
+
+    int ary1[5] = { 10, 20, 30, 40, 50};
+    int ary2[7] = {10,20,30,40,50,60,70};
+
+    printf_ary(ary1,5);
+    printf("\n");
+    printf_ary(ary2, 7);
+
+    return 0;
+}
+
+void print_ary(int *pa, int size){
+    int i;
+
+    for(i=0; i < size; i++){
+        printf("%d ", pa[i]);
+    }
+}
